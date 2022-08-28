98acd6647b711e0cf9d5cfe61209b175fe49447f
Nahuel Pereira <110473467+nahuper@users.noreply.github.com>
2022-08-28 15:34:10 -0300

    Actualización listado de productos

--- js/login.js
+++ js/login.js
@@ -21,7 +21,7 @@ btn.addEventListener('click', function(){
         
         document.getElementById("respuesta").innerHTML =  '<div class="border border-success p-2 mb-2">Loging in</div>';
         localStorage.setItem("username", username);
-        location.href="../marketplace/index.html";
+        location.href="../index.html";
         
     }else{
         
@@ -33,35 +33,6 @@ btn.addEventListener('click', function(){
 });
 
 
-/*if(btn){
-    btn.addEventListener('click', function(){
-
-        //e.preventDefault();
-        //console.log(e.target);
-        const username = document.getElementById("fieldEmail").value;
-        const password = document.getElementById("fieldContrasenia").value;
-    
-        localStorage.setItem(username, password);
-        
-        if(username!="" && password!=""){
-            document.getElementById("respuesta").innerHTML =  '<div class="border border-success p-2 mb-2">Loging in</div>';
-            localStorage.setItem("username", username);
-            //document.getElementById("usrName").innerHTML = nombreUsuario
-            
-            obtener_localstorage();
-            location.href="../index.html";
-            
-        }else{
-            localStorage.clear();
-            document.getElementById("respuesta").innerHTML =  '<div class="h4 pb-2 mb-4 text-danger border-bottom border-danger">No ingresó ningún dato</div>';
-            
-        }
-    
-        
-    });
-}else{
-    console.log("ERROR")
-}*/
 
 
 
--- js/products.js
+++ js/products.js
@@ -1,14 +1,21 @@
 let list = [];
+const btnFiltrar = document.getElementById("rangeFilterCount");
+const btnLimpiar = document.getElementById("clearRangeFilter");
+const max = document.getElementById("rangeFilterCountMax");
+const min = document.getElementById("rangeFilterCountMin"); 
 
 
 
 const urlDinamico = localStorage.getItem("urlFetch");
 const PRODUCTS_COLLECTION = urlDinamico;
 
+mostrarItems();
+
 fetch(PRODUCTS_COLLECTION)
+
 .then(response => {return response.json()})
 .then(data=>{
-
+    
 
     /*Se obtiene del JSON formateado (legible) el dato del nombre de la categoría
     y se lo pasa al HTML para mostrarlo en la categoría seleccionada*/ 
@@ -29,15 +36,72 @@ el HTML categoría por categoría*/
         list.push(data.products[dato]);
     }
     
+    mostrarItems();
 
-let htmlContent="";
+})
+.catch(error=>{
+    console.log(error)
+});
+
+
+btnFiltrar.addEventListener("click", ()=>{
+
+    let htmlContent="";
+
+    let dato=0;
+
+    for(dato of list){
+        if(dato.cost >= min.value && dato.cost <= max.value){
+            console.log(dato.soldCount);
+            htmlContent += `
+
+    
+            <div class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + dato.image + `" alt="product image" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <div class="mb-1">
+                            <h2> `+ dato.name +`</h2>
+                            <p> `+ dato.description +`</p>
+                            <h4> U$D `+ dato.cost +`</h4>
+                            </div>
+                            <small class=text-muted> Cantidad venididos: `+ dato.soldCount +`</small>
+                            
+                        </div>
+                        </div>
+                    </div>
+                </div>                
+            `
+            document.getElementById("products-list").innerHTML = htmlContent;
+            
+        }else if(min.value==="" && max.value===""){
+            mostrarItems();
+            console.log("error");
+        }
+        
+    }
+});
+
+btnLimpiar.addEventListener("click", ()=>{
+    mostrarItems();
+
+});
+
+function mostrarItems(){
+    let htmlContent="";
+
+
+    
 
 for(let i=0; i<list.length; i++){
 
     
     let items = list[i];
-    
-        htmlContent += `
+
+    htmlContent += `
 
     
             <div class="list-group-item list-group-item-action">
@@ -60,16 +124,13 @@ for(let i=0; i<list.length; i++){
                 </div>                
             `
             document.getElementById("products-list").innerHTML = htmlContent;
-}
-   
-   
+    
+    
+        
+};
+};
 
 
 
-            
 
-})
-.catch(error=>{
-    console.log(error)
-})
 
