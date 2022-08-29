8268bb11b4409f29f4c46052009f30bec7d65031
Nahuel Pereira <nprep2018@gmail.com>
2022-08-29 11:03:13 -0300

    se muestra el nombre de usuario en los menúes superiores

--- js/login.js
+++ js/login.js
@@ -21,7 +21,7 @@ btn.addEventListener('click', function(){
         
         document.getElementById("respuesta").innerHTML =  '<div class="border border-success p-2 mb-2">Loging in</div>';
         localStorage.setItem("username", username);
-        location.href="../marketplace/index.html";
+        location.href="../index.html";
         
     }else{
         
