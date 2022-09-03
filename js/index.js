0a2a83da4bf79d40fd3649673229fc09f63144fa
Nahuel Pereira <110473467+nahuper@users.noreply.github.com>
2022-09-03 10:31:15 -0300

    corrección de errores

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    window.location="/marketplace/login.html"
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
