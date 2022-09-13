8338e8c2b467c647463273dea008ced469fc70d4
Nahuel Pereira <nprep2018@gmail.com>
2022-09-12 22:31:17 -0300

    arreglo de bug de seguridad a nivel de acceso

--- js/product-info.js
+++ js/product-info.js
@@ -11,187 +11,194 @@ const btnPublish = document.getElementById("btnPublish");
 let publicationList = [];
 let cargaComentarios="";
 
-btnPublish.addEventListener("click", ()=>{
-    const select = document.getElementById("scores");
-    const valueAreaText = document.getElementById("textValue").value;
-    const valueMenuScore = select.options[select.selectedIndex].value;
 
-    let today = new Date();
-    let nowDate = today.toLocaleDateString();
-    let nowTime = today.toLocaleTimeString();
+if(localStorage.getItem("username")===null){
+    location.href="../marketplace/login.html";
+}else{
+    btnPublish.addEventListener("click", ()=>{
+        const select = document.getElementById("scores");
+        const valueAreaText = document.getElementById("textValue").value;
+        const valueMenuScore = select.options[select.selectedIndex].value;
     
-    const actualDate = nowDate;
-    const actualHour = nowTime;
-
-    const itemList = JSON.parse(localStorage.getItem("listCommentsObj"));
-
+        let today = new Date();
+        let nowDate = today.toLocaleDateString();
+        let nowTime = today.toLocaleTimeString();
+        
+        const actualDate = nowDate;
+        const actualHour = nowTime;
     
-
-    if(valueAreaText !== null){
-           
-            const comment = [{
-                text: valueAreaText,
-                date: actualDate,
-                hour: actualHour,
-                score: valueMenuScore
-            }]
-            publicationList = comment;
+        const itemList = JSON.parse(localStorage.getItem("listCommentsObj"));
+    
+        
+    
+        if(valueAreaText !== null){
+               
+                const comment = [{
+                    text: valueAreaText,
+                    date: actualDate,
+                    hour: actualHour,
+                    score: valueMenuScore
+                }]
+                publicationList = comment;
+                localStorage.setItem("listCommentsObj", JSON.stringify(publicationList));
+                valueAreaText.value = "";
+                console.log(JSON.parse(localStorage.getItem("listCommentsObj")));
+        }else if(valueAreaText === null){
+            let listOfItems=[itemList];
             localStorage.setItem("listCommentsObj", JSON.stringify(publicationList));
             valueAreaText.value = "";
-            console.log(JSON.parse(localStorage.getItem("listCommentsObj")));
-    }else if(valueAreaText === null){
-        let listOfItems=[itemList];
-        localStorage.setItem("listCommentsObj", JSON.stringify(publicationList));
-        valueAreaText.value = "";
-    }
-  
-    
-    //console.log(publicationList[0]);
-
-    /*console.log(nowDate);
-    console.log(nowTime);
-    console.log(valueAreaText);
-
-    console.log(valueMenuScore);*/
-
-
-});
-
-fetch(urlComments)
-    .then((result)=>{return result.json()})
-    .then((dat) => {
-        
-        cargaComentarios = "";
-        for(dato in dat){
-            let item = dat[dato];
-            //console.log(item.score);
-            cargaComentarios += `
-            <hr style="color:black; background-color:black; width:75%;">
-            <strong>${item.user}</strong>
-            ${item.dateTime}
-            
-            <p>${item.description}</p>
-            `
-
-            for(let i=0; i<item.score; i++){
-
-                cargaComentarios += `
-                        <span class="fa fa-star checked"></span>
-                
-                `
-                
-            }
-
-            document.getElementById("comments-container").innerHTML = cargaComentarios;
         }
-    })
-    .catch(error=>{
-        console.log(error);
-    });
-
-    //showStars();
-
-    /*function showStars(item){
-        const listComments = "https://japceibal.github.io/emercado-api/products_comments/";
-        const jsonFormat = ".json";
-        const id = localStorage.getItem("prodID");
-        const urlFormed = listComments + id + jsonFormat;
-        counter = item.id[0]; //captura el primer caracter
-        let name = item.id.substring(1); // captura todo menos el primer caracter
-        console.log(item);
+      
         
-        fetch(urlFormed)
+        //console.log(publicationList[0]);
+    
+        /*console.log(nowDate);
+        console.log(nowTime);
+        console.log(valueAreaText);
+    
+        console.log(valueMenuScore);*/
+    
+    
+    });
+    
+    fetch(urlComments)
         .then((result)=>{return result.json()})
-        .then((data)=>{
-          for(let score in data){
-            let item = data[score];
-            //console.log(item.score);
-            for(let i=0; i<5; i++){
-                if(i<counter){
-                    document.getElementById((i+1)+name).style.color="orange";
-                }
+        .then((dat) => {
+            
+            cargaComentarios = "";
+            for(dato in dat){
+                let item = dat[dato];
+                //console.log(item.score);
+                cargaComentarios += `
+                <hr style="color:black; background-color:black; width:75%;">
+                <strong>${item.user}</strong>
+                ${item.dateTime}
                 
-            }
-          }
-          
-          
-        })
-    };*/
-
-fetch(urlFormateado)
-    .then((res) => {return res.json()})
-    .then((data) => {
-
-        let camposHTML = "";
-        let imagenes = "";
-        console.log(data)
-        
-        
-        /*Se obtiene del JSON formateado (legible) el dato del nombre del producto
-        y se lo pasa al HTML para mostrarlo la página de detalles del producto seleccionado*/ 
-        let titulo="";
-
-        titulo +=`<h2>${data.name}</h2>`
-
-
-        document.getElementById("tipoDeProducto").innerHTML = titulo;
-
-        console.log(data.catName);
-        
-
-        camposHTML += `<div class="col">
-            <div class="d-flex w-100 justify-content-between">
-                <div class="mb-1">
-                <h2> Precio </h2>
-                <p> UYU`+ data.cost +`</p>
-                <h2> Description </h2>
-                <p>` + data.description + `</p>
-                <h2> Categoría </h2>
-                <p>` + data.category + `</p>
-                <h2> Cantidad vendidos </h2>
-                <p>` + data.soldCount + `</p>
-                <h2>Imágenes ilustrativas:</h2>
-                </div>
-
-                
-            </div>
-            </div>`
-
-            for(let dat in data.images){
-
-                imagenes += `
-
-                <div class="row">
-                <div class="column" id="img">
-                    <img class="img-thumbnail img-fluid" src="${data.images[dat]}" class="img-thumbnail">   
-                </div>
-                </div>
-                
-                <style>
-                    img{
-                        width: 30%;
-                    }
-                    .row{
-                        display: flex;
-                        padding: 5px;
-                        flex-wrap: wrap;
-                    }
-                    #id{
-                        flex: 25%;
-                        padding: 5px;
-                    }
-                </style>
-                
+                <p>${item.description}</p>
                 `
-                console.log(data.images[dat])
+    
+                for(let i=0; i<item.score; i++){
+    
+                    cargaComentarios += `
+                            <span class="fa fa-star checked"></span>
+                    
+                    `
+                    
+                }
+    
+                document.getElementById("comments-container").innerHTML = cargaComentarios;
             }
+        })
+        .catch(error=>{
+            console.log(error);
+        });
+    
+        //showStars();
+    
+        /*function showStars(item){
+            const listComments = "https://japceibal.github.io/emercado-api/products_comments/";
+            const jsonFormat = ".json";
+            const id = localStorage.getItem("prodID");
+            const urlFormed = listComments + id + jsonFormat;
+            counter = item.id[0]; //captura el primer caracter
+            let name = item.id.substring(1); // captura todo menos el primer caracter
+            console.log(item);
+            
+            fetch(urlFormed)
+            .then((result)=>{return result.json()})
+            .then((data)=>{
+              for(let score in data){
+                let item = data[score];
+                //console.log(item.score);
+                for(let i=0; i<5; i++){
+                    if(i<counter){
+                        document.getElementById((i+1)+name).style.color="orange";
+                    }
+                    
+                }
+              }
+              
+              
+            })
+        };*/
+    
+    fetch(urlFormateado)
+        .then((res) => {return res.json()})
+        .then((data) => {
+    
+            let camposHTML = "";
+            let imagenes = "";
+            console.log(data)
+            
+            
+            /*Se obtiene del JSON formateado (legible) el dato del nombre del producto
+            y se lo pasa al HTML para mostrarlo la página de detalles del producto seleccionado*/ 
+            let titulo="";
+    
+            titulo +=`<h2>${data.name}</h2>`
+    
+    
+            document.getElementById("tipoDeProducto").innerHTML = titulo;
+    
+            console.log(data.catName);
+            
+    
+            camposHTML += `<div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <div class="mb-1">
+                    <h2> Precio </h2>
+                    <p> UYU`+ data.cost +`</p>
+                    <h2> Description </h2>
+                    <p>` + data.description + `</p>
+                    <h2> Categoría </h2>
+                    <p>` + data.category + `</p>
+                    <h2> Cantidad vendidos </h2>
+                    <p>` + data.soldCount + `</p>
+                    <h2>Imágenes ilustrativas:</h2>
+                    </div>
+    
+                    
+                </div>
+                </div>`
+    
+                for(let dat in data.images){
+    
+                    imagenes += `
+    
+                    <div class="row">
+                    <div class="column" id="img">
+                        <img class="img-thumbnail img-fluid" src="${data.images[dat]}" class="img-thumbnail">   
+                    </div>
+                    </div>
+                    
+                    <style>
+                        img{
+                            width: 30%;
+                        }
+                        .row{
+                            display: flex;
+                            padding: 5px;
+                            flex-wrap: wrap;
+                        }
+                        #id{
+                            flex: 25%;
+                            padding: 5px;
+                        }
+                    </style>
+                    
+                    `
+                    console.log(data.images[dat])
+                }
+    
+            document.getElementById("dataProduct").innerHTML = camposHTML;
+            document.getElementById("imgs").innerHTML = imagenes;
+        })
+        .catch(error=>{
+            console.log(error)
+        });
+}
 
-        document.getElementById("dataProduct").innerHTML = camposHTML;
-        document.getElementById("imgs").innerHTML = imagenes;
-    })
-    .catch(error=>{
-        console.log(error)
-    });
+
 
 
 
--- js/products.js
+++ js/products.js
@@ -11,239 +11,244 @@ const cuadroBusqueda = document.getElementById("busqueda");
 const PRODUCTS_COLLECTION = urlDinamico;
 
 
-function setProdId(id){
-    localStorage.setItem("prodID", id);
-    window.location = "product-info.html";
-}
-
-
-
-/** Aqui se filtra el listado en base a lo que se escribe en el imput de búsqueda 
- * se recorra la lista de productos y se evalua que el texto ingresado en el cuadro de búsqueda
- * coincida con el título y la descripción del item.
-*/
-cuadroBusqueda.addEventListener("keyup", ()=>{
-    //console.log(cuadroBusqueda.value);
-    let texto = cuadroBusqueda.value.toLowerCase();
-    texto = quitarAcentos(texto);
-    let htmlContent="";
-    for(let producto of list){
-        let nom = producto.name.toLowerCase();
-        let descr = producto.description.toLowerCase();
-
-        if(nom.indexOf(texto) !== -1 || descr.indexOf(texto) !== -1){
-            htmlContent += `
-
-    
-            <div onclick="setProdId(${producto.id})" class="list-group-item list-group-item-action cursor-active">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` + producto.image + `" alt="product image" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <div class="mb-1">
-                            <h2> `+ producto.name +`</h2>
-                            <p> `+ producto.description +`</p>
-                            <h4> U$D `+ producto.cost +`</h4>
-                            </div>
-                            <small class=text-muted> Cantidad venididos: `+ producto.soldCount +`</small>
-                            
-                        </div>
-                        </div>
-                    </div>
-                </div>                 
-            `
-            
-        }
-    }
-
-    
-    document.getElementById("products-list").innerHTML = htmlContent;
-
-});
-
-
-/**Con este método se quitan los acentos del texto ingresado */
-
-function quitarAcentos(string) {
-
-    return string.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-}
-
-
-
-
-/** Se listan los items de forma descendiante en base a la cantidad de ventas */
-descByCount.addEventListener("click", ()=>{
-    
-    
-    list.sort(function(a,b){
-        if(a.soldCount>b.soldCount){
-            return -1;
-        }
-        if(a.soldCount<navigator.soldCount){
-            return 1;
-        }
-        return 0;
-    });
-    mostrarItems();
-
-});
-
-/** Se listan los items de forma ascendente en base al costo del producto */
-asc.addEventListener("click", ()=>{
-    
-    
-    list.sort(function (a, b){
-        if(a.cost > b.cost){
-            return 1;
-        }
-        if(a.cost < b.cost){
-            return -1;
-        }
-        return 0;
-
-    });
-    mostrarItems();
-});
-
-
-/**Se listan los items de forma descendente en base al costo */
-desc.addEventListener("click", ()=>{
-    
-    
-    list.sort(function (a, b){
-        if(a.cost > b.cost){
-            return -1;
-        }
-        if(a.cost < b.cost){
-            return 1;
-        }
-        return 0;
-
-    });
-    mostrarItems();
-});
-
-mostrarItems();
-
-fetch(PRODUCTS_COLLECTION)
-
-.then(response => {return response.json()})
-.then(data=>{
-    
-
-    /*Se obtiene del JSON formateado (legible) el dato del nombre de la categoría
-    y se lo pasa al HTML para mostrarlo en la categoría seleccionada*/ 
-    let titulo="";
-
-    titulo +=`<h2>${data.catName}</h2>`
-
-
-    document.getElementById("tituloArticulo").innerHTML = titulo;
-
-    console.log(data.catName);
-    
-    
-/*Se recorre los productos dentro de una categoría dada y se muestran en 
-el HTML categoría por categoría*/
-
-    for(let dato in data.products){
-        list.push(data.products[dato]);
+if(localStorage.getItem("username")===null){
+    location.href="../marketplace/login.html";
+}else{
+    function setProdId(id){
+        localStorage.setItem("prodID", id);
+        window.location = "product-info.html";
     }
     
-    mostrarItems();
-
-})
-.catch(error=>{
-    console.log(error)
-});
-
-
-btnFiltrar.addEventListener("click", ()=>{
-
-    let htmlContent="";
-
-    let dato=0;
-
-    for(dato of list){
-        if(dato.cost >= min.value && dato.cost <= max.value || dato.cost >=max.value && dato.cost <= min.value){
-            console.log(dato.cost);
-            htmlContent += `
-
-            <div onclick="setProdId(${dato.id})" class="list-group-item list-group-item-action cursor-active">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` + dato.image + `" alt="product image" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <div class="mb-1">
-                            <h2> `+ dato.name +`</h2>
-                            <p> `+ dato.description +`</p>
-                            <h4> U$D `+ dato.cost +`</h4>
-                            </div>
-                            <small class=text-muted> Cantidad venididos: `+ dato.soldCount +`</small>
-                            
-                        </div>
-                        </div>
-                    </div>
-                </div>                
-            `
-            document.getElementById("products-list").innerHTML = htmlContent;
-            
-        }else if(min.value==="" && max.value===""){
-            mostrarItems();
-            console.log("error");
-        }
+    
+    
+    /** Aqui se filtra el listado en base a lo que se escribe en el imput de búsqueda 
+     * se recorra la lista de productos y se evalua que el texto ingresado en el cuadro de búsqueda
+     * coincida con el título y la descripción del item.
+    */
+    cuadroBusqueda.addEventListener("keyup", ()=>{
+        //console.log(cuadroBusqueda.value);
+        let texto = cuadroBusqueda.value.toLowerCase();
+        texto = quitarAcentos(texto);
+        let htmlContent="";
+        for(let producto of list){
+            let nom = producto.name.toLowerCase();
+            let descr = producto.description.toLowerCase();
+    
+            if(nom.indexOf(texto) !== -1 || descr.indexOf(texto) !== -1){
+                htmlContent += `
+    
         
-    }
-});
-
-btnLimpiar.addEventListener("click", ()=>{
-    mostrarItems();
-    max.value = "";
-    min.value = "";
-
-});
-
-function mostrarItems(){
-    let htmlContent="";
-
-
-    
-
-for(let i=0; i<list.length; i++){
-
-    
-    let items = list[i];
-
-    htmlContent += `
-
-    
-    <div onclick="setProdId(${items.id})" class="list-group-item list-group-item-action cursor-active">
-    <div class="row">
-        <div class="col-3">
-            <img src="` + items.image + `" alt="product image" class="img-thumbnail">
-        </div>
-        <div class="col">
-            <div class="d-flex w-100 justify-content-between">
-                <div class="mb-1">
-                <h2> `+ items.name +`</h2>
-                <p> `+ items.description +`</p>
-                <h4> U$D `+ items.cost +`</h4>
-                </div>
-                <small class=text-muted> Cantidad venididos: `+ items.soldCount +`</small>
+                <div onclick="setProdId(${producto.id})" class="list-group-item list-group-item-action cursor-active">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="` + producto.image + `" alt="product image" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <div class="mb-1">
+                                <h2> `+ producto.name +`</h2>
+                                <p> `+ producto.description +`</p>
+                                <h4> U$D `+ producto.cost +`</h4>
+                                </div>
+                                <small class=text-muted> Cantidad venididos: `+ producto.soldCount +`</small>
+                                
+                            </div>
+                            </div>
+                        </div>
+                    </div>                 
+                `
                 
-            </div>
-            </div>
-        </div>
-    </div>                
-            `
-            document.getElementById("products-list").innerHTML = htmlContent;
+            }
+        }
+    
+        
+        document.getElementById("products-list").innerHTML = htmlContent;
+    
+    });
+    
+    
+    /**Con este método se quitan los acentos del texto ingresado */
+    
+    function quitarAcentos(string) {
+    
+        return string.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    }
+    
+    
+    
+    
+    /** Se listan los items de forma descendiante en base a la cantidad de ventas */
+    descByCount.addEventListener("click", ()=>{
+        
+        
+        list.sort(function(a,b){
+            if(a.soldCount>b.soldCount){
+                return -1;
+            }
+            if(a.soldCount<navigator.soldCount){
+                return 1;
+            }
+            return 0;
+        });
+        mostrarItems();
+    
+    });
+    
+    /** Se listan los items de forma ascendente en base al costo del producto */
+    asc.addEventListener("click", ()=>{
+        
+        
+        list.sort(function (a, b){
+            if(a.cost > b.cost){
+                return 1;
+            }
+            if(a.cost < b.cost){
+                return -1;
+            }
+            return 0;
+    
+        });
+        mostrarItems();
+    });
+    
+    
+    /**Se listan los items de forma descendente en base al costo */
+    desc.addEventListener("click", ()=>{
+        
+        
+        list.sort(function (a, b){
+            if(a.cost > b.cost){
+                return -1;
+            }
+            if(a.cost < b.cost){
+                return 1;
+            }
+            return 0;
+    
+        });
+        mostrarItems();
+    });
+    
+    mostrarItems();
+    
+    fetch(PRODUCTS_COLLECTION)
+    
+    .then(response => {return response.json()})
+    .then(data=>{
+        
+    
+        /*Se obtiene del JSON formateado (legible) el dato del nombre de la categoría
+        y se lo pasa al HTML para mostrarlo en la categoría seleccionada*/ 
+        let titulo="";
+    
+        titulo +=`<h2>${data.catName}</h2>`
+    
+    
+        document.getElementById("tituloArticulo").innerHTML = titulo;
+    
+        console.log(data.catName);
+        
+        
+    /*Se recorre los productos dentro de una categoría dada y se muestran en 
+    el HTML categoría por categoría*/
+    
+        for(let dato in data.products){
+            list.push(data.products[dato]);
+        }
+        
+        mostrarItems();
+    
+    })
+    .catch(error=>{
+        console.log(error)
+    });
+    
+    
+    btnFiltrar.addEventListener("click", ()=>{
+    
+        let htmlContent="";
+    
+        let dato=0;
+    
+        for(dato of list){
+            if(dato.cost >= min.value && dato.cost <= max.value || dato.cost >=max.value && dato.cost <= min.value){
+                console.log(dato.cost);
+                htmlContent += `
+    
+                <div onclick="setProdId(${dato.id})" class="list-group-item list-group-item-action cursor-active">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="` + dato.image + `" alt="product image" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <div class="mb-1">
+                                <h2> `+ dato.name +`</h2>
+                                <p> `+ dato.description +`</p>
+                                <h4> U$D `+ dato.cost +`</h4>
+                                </div>
+                                <small class=text-muted> Cantidad venididos: `+ dato.soldCount +`</small>
+                                
+                            </div>
+                            </div>
+                        </div>
+                    </div>                
+                `
+                document.getElementById("products-list").innerHTML = htmlContent;
+                
+            }else if(min.value==="" && max.value===""){
+                mostrarItems();
+                console.log("error");
+            }
+            
+        }
+    });
+    
+    btnLimpiar.addEventListener("click", ()=>{
+        mostrarItems();
+        max.value = "";
+        min.value = "";
+    
+    });
+    
+    function mostrarItems(){
+        let htmlContent="";
     
     
         
-};
-};
+    
+    for(let i=0; i<list.length; i++){
+    
+        
+        let items = list[i];
+    
+        htmlContent += `
+    
+        
+        <div onclick="setProdId(${items.id})" class="list-group-item list-group-item-action cursor-active">
+        <div class="row">
+            <div class="col-3">
+                <img src="` + items.image + `" alt="product image" class="img-thumbnail">
+            </div>
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <div class="mb-1">
+                    <h2> `+ items.name +`</h2>
+                    <p> `+ items.description +`</p>
+                    <h4> U$D `+ items.cost +`</h4>
+                    </div>
+                    <small class=text-muted> Cantidad venididos: `+ items.soldCount +`</small>
+                    
+                </div>
+                </div>
+            </div>
+        </div>                
+                `
+                document.getElementById("products-list").innerHTML = htmlContent;
+        
+        
+            
+    };
+    };
+}
+
