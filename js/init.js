39a3429b3fc026ad7e180c6c926e1e5f89d98d75
Nahuel Pereira <110473467+nahuper@users.noreply.github.com>
2022-09-22 11:20:11 -0300

    arreglo de bug al cerrar sesión

--- js/init.js
+++ js/init.js
@@ -59,7 +59,7 @@ let getJSONData = function(url){
 
   function closeSession(){
     localStorage.removeItem("username");
-    location.href="../login.html";
+    location.href="../marketplace/login.html";
   }
   //console.log(userNameReturn());
 
