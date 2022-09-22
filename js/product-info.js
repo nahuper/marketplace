2e507792dcec574edfe6229abe3e852d613eeecd
Nahuel Pereira <nprep2018@gmail.com>
2022-09-22 16:17:04 -0300

    agregado sistema carrusel

--- js/product-info.js
+++ js/product-info.js
@@ -145,7 +145,37 @@ if(localStorage.getItem("username")===null){
     function showImagesOfProduct(data){
         let imagenes = "";
         
-        for(let dat in data.images){
+        imagenes += `
+        <div id="carouselExampleControls" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="5000">
+                    <img src="${data.images[0]}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="5000">
+                    <img src="${data.images[1]}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="5000">
+                    <img src="${data.images[2]}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="5000">
+                    <img src="${data.images[3]}" class="d-block w-100" alt="...">
+                </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        </div>
+        `
+        document.getElementById("imgs").innerHTML = imagenes;
+        /*for(let dat in data.images){
+
+            console.log(dat);
+            console.log(data.images[0]);
             imagenes += `
 
                 <div class="row">
@@ -171,8 +201,7 @@ if(localStorage.getItem("username")===null){
                 
                 `
                 
-        }
-        document.getElementById("imgs").innerHTML = imagenes;
+        }*/
     }
   
 
